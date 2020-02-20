e27f19422f40ed890f676494b5b54339d482aff5
linggaralfi <linggaralfithna@gmail.com>
2020-02-20 13:17:04 +0700

    project pertama

---
+++ app/src/main/java/com/linggar/mynote/MainActivity.java
@@ -0,0 +1,14 @@
+package com.linggar.mynote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
